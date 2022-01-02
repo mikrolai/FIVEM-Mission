71d6c2ee94c1ce47328a1ceb885fc899dcbe54db
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2022-01-02 14:13:09 +0100

    Missionen auf dem Server starten.
    Missionen sollten nun auf dem Server "startbar" sein.

--- config.lua
+++ config.lua
@@ -10,9 +10,13 @@ CFG.Farben.Rot = 6
 
 CFG.Gegnaz = {}
 
+CFG.Marker = {}
+CFG.Marker.Wolfskopf = 31
+
 CFG.Missionen = {}
 CFG.Missionen[1] = {
 	name = "testmission",
 	gegneranzahl = 1,
-	startpoint = { x = 2492.934, y = 5108.829, z = 45.16, heading = 320.0 }
+	startpoint = { x = 2492.934, y = 5108.829, z = 45.16, heading = 320.0 }, 
+	marker = CFG.Marker.Wolfskopf
 }
--- missionserver.lua
+++ missionserver.lua
@@ -4,12 +4,12 @@ AddEventHandler('onResourceStart', function(resourceName)
   end
   print('Erstelle Marker')  
   for i = 1, #CFG.Missionen, 1 do
-	DrawMarker(31, CFG.Missionen[i].startpoint.x, CFG.Missionen[i].startpoint.y, CFG.Missionen[i].startpoint.z + 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 0, 0, 255, 50, true, true, 2, nil, nil, false)
+	DrawMarker(CFG.Missionen[i].marker, CFG.Missionen[i].startpoint.x, CFG.Missionen[i].startpoint.y, CFG.Missionen[i].startpoint.z + 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 0, 0, 255, 50, true, true, 2, nil, nil, false)
   end
   print('Alle Marker erstellt')  
 end)
 
-RegisterServerEvent('first_point:Blip')
-AddEventHandler('first_point:Blip', function()
-    TriggerClientEvent('first_point:Blip')
+RegisterServerEvent('missionen:start')
+AddEventHandler('missionen:start', function(missionsnummer)
+	print('Mission ' .. missionsnummer .. ' (' .. CFG.Missionen[i].name .. ') gestartet!')
 end)
